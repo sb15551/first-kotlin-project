440823a16836df8d5d906723badfe9cc749b1e76
Сурков Алексей Анатольевич <stibium128@gmail.com>
2026-01-24 18:04:42 +0400

    KS-1-5 calc time Gagarin spent in space

---
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -0,0 +1,15 @@
+package ru.behavior.lesson_1
+
+fun main() {
+
+    val SECONDS_TO_HOUR: Int = 3600
+    val SECONDS_TO_MINUTE: Int = 60
+
+    var secondsInSpace: Int = 6480
+    var hours: Int = secondsInSpace / SECONDS_TO_HOUR
+    var minutes: Int = (secondsInSpace % SECONDS_TO_HOUR) / SECONDS_TO_MINUTE
+    secondsInSpace %= SECONDS_TO_MINUTE
+
+    println("The time Gagarin spent in space: ${"%02d:%02d:%02d".format(hours, minutes, secondsInSpace)}")
+
+}
