277bded9c2ccff1f5f569443f78ebaa8e9234883
Сурков Алексей Анатольевич <stibium128@gmail.com>
2026-01-24 17:38:40 +0400

    KS-1-4 print data about Gagarin

---
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -0,0 +1,19 @@
+package ru.behavior.lesson_1
+
+fun main() {
+
+    val length: Long = 40_868_600_000
+    var age: Short = 27
+    var partOfDay: Double = 0.075
+    var seconds: Int = 6480
+    var partOfYear: Double = 2.0547945205479453E-4
+    var apogee: Int = 327000
+
+    println("The distance Gagarin flew in space in millimeters: $length")
+    println("Gagarin's age at the time of the flight (in years): $age")
+    println("The raction of the day Gagarin spent in space: $partOfDay")
+    println("The number of seconds Gagarin spent in space: $seconds")
+    println("The fraction of the year Gagarin spent in space: $partOfYear")
+    println("The apogee of Gagarin's orbit in meters: $apogee")
+
+}
