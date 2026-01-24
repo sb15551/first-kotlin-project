1b079f67dbb85dbf614bfd5658c8f54b7872c56a
Сурков Алексей Анатольевич <stibium128@gmail.com>
2026-01-24 16:30:32 +0400

    KS-1-2 add count employee

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,15 @@
+package ru.behavior.lesson_1
+
+fun main() {
+
+    val countOrders: Int = 75
+    val textOfGratitude = "Thank you for your order!"
+
+    println("Count order = $countOrders")
+    println(textOfGratitude)
+
+//    val countEmployees: Int = 2000
+    val countEmployees: Int = 1999
+    println("Count employees = $countEmployees")
+
+}
