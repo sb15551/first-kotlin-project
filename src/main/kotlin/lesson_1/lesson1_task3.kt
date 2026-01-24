9d817949e051edbe7e66d5b5a206e592acbe7549
Сурков Алексей Анатольевич <stibium128@gmail.com>
2026-01-24 16:42:54 +0400

    KS-1-3 print data about first human spaceflight

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,18 @@
+package ru.behavior.lesson_1
+
+fun main() {
+
+    val yearFirstHumanSpaceflight: Int = 1961
+    var hourFirstHumanSpaceflight: Int = 9
+    var minuteFirstHumanSpaceflight: Int = 7
+
+    println("Year first human spaceflight = $yearFirstHumanSpaceflight")
+    println("Hour first human spaceflight = $hourFirstHumanSpaceflight")
+    println("Minute first human spaceflight = $minuteFirstHumanSpaceflight")
+
+    hourFirstHumanSpaceflight = 10
+    minuteFirstHumanSpaceflight = 55
+
+    println("Boarding time = $hourFirstHumanSpaceflight:$minuteFirstHumanSpaceflight")
+
+}
