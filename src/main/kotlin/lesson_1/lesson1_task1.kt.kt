802afaeab34c9f125f193a85599fc7e2eb50dcb4
Сурков Алексей Анатольевич <stibium128@gmail.com>
2026-01-24 14:43:43 +0400

    KS-1-1 add variables

---
+++ src/main/kotlin/lesson_1/lesson1_task1.kt.kt
@@ -0,0 +1,11 @@
+package ru.behavior.lesson_1
+
+fun main() {
+
+    val countOrders: Int = 75
+    val textOfGratitude = "Thank you for your order!"
+
+    println("Count order = $countOrders")
+    println(textOfGratitude)
+
+}
