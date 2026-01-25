88a5cb74f83bc99cb1409a9ca54938c525453eb2
Сурков Алексей Анатольевич <stibium128@gmail.com>
2026-01-25 16:22:45 +0400

    KS-1-4 resolve comments

--- src/main/kotlin/lesson_1/lesson1_task4.kt
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -3,11 +3,11 @@ package ru.behavior.lesson_1
 fun main() {
 
     val length: Long = 40_868_600_000
-    var age: Short = 27
-    var partOfDay: Double = 0.075
-    var seconds: Int = 6480
-    var partOfYear: Double = 2.0547945205479453E-4
-    var apogee: Int = 327000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075f
+    val seconds: Int = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
 
     println("The distance Gagarin flew in space in millimeters: $length")
     println("Gagarin's age at the time of the flight (in years): $age")
